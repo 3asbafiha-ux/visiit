bcbc65a6996d0ac621a5618550af121ead63f5a1
kaledmhdkl <isameryem212@gmail.com>
2025-08-23 22:58:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def send_visit():
 
     # حلقة لإرسال زيارات حتى الوصول لـ 500 زيارة ناجحة
     while len(results) < 500:
-        batch_tokens = tokens[index:index+500]  # نأخذ حتى 500 توكن لكل batch
+        batch_tokens = tokens[index:index+1000]  # نأخذ حتى 500 توكن لكل batch
         if not batch_tokens:
             break  # إذا نفذت التوكنات
 
@@ -114,13 +114,13 @@ def send_visit():
             success = send_visit_request(token, TARGET)
             return {"token": token[:20]+"...", "status": "success" if success else "failed"}
 
-        with ThreadPoolExecutor(max_workers=1000) as executor:
+        with ThreadPoolExecutor(max_workers=500) as executor:
             futures = [executor.submit(worker, t) for t in batch_tokens]
             for future in as_completed(futures):
                 res = future.result()
                 if res["status"] == "success":
                     results.append(res)
-                if len(results) >= 1000:
+                if len(results) >= 500:
                     break
 
         index += 500
